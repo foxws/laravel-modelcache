c5fb9ca60ef36c8798207cb0c6890b206cd01208
francoism90 <francoism90@users.noreply.github.com>
2024-10-27 21:16:06 +0100

    wip

--- tests/src/Hashers/CacheHasherTest.php
+++ tests/src/Hashers/CacheHasherTest.php
@@ -2,6 +2,7 @@
 
 use Foxws\UserCache\CacheProfiles\CacheProfile;
 use Foxws\UserCache\Hasher\DefaultHasher;
+use Foxws\UserCache\Tests\Models\User;
 use Foxws\UserCache\Tests\TestCase;
 
 use function PHPUnit\Framework\assertEquals;
@@ -9,6 +10,8 @@ use function PHPUnit\Framework\assertEquals;
 uses(TestCase::class);
 
 beforeEach(function () {
+    $this->user = User::factory()->create();
+
     $this->cacheProfile = Mockery::mock(CacheProfile::class);
 
     $this->cacheHasher = new DefaultHasher($this->cacheProfile);
@@ -19,6 +22,12 @@ it('can generate a hash for a cache', function () {
 
     assertEquals(
         'usercache-5d712697b89eb7b5e319c4faf4272cf5',
-        $this->cacheHasher->getHashFor('last_viewed')
+        $this->cacheHasher->getHashFor($this->user, 'last_viewed')
     );
+});
+
+it('can store value using user store', function () {
+    $this->user->cacheStore('cacheKey', 'cacheValue');
+
+    assertEquals('cacheValue', $this->user->cacheStored('cacheKey'));
 });
