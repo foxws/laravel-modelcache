be6f863482549fc78328b45d56f3b4dcf5dfaea2
francoism90 <francoism90@users.noreply.github.com>
2024-10-27 17:10:37 +0100

    wip

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,18 +3,32 @@
 namespace Foxws\UserCache\Tests;
 
 use Foxws\UserCache\UserCacheServiceProvider;
-use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Orchestra\Testbench\Concerns\WithWorkbench;
 use Orchestra\Testbench\TestCase as Orchestra;
 
-class TestCase extends Orchestra
+abstract class TestCase extends Orchestra
 {
+    use WithWorkbench;
+
     protected function setUp(): void
     {
         parent::setUp();
 
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'Foxws\\UserCache\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
+        $this->setUpDatabase($this->app);
+    }
+
+    protected function getEnvironmentSetUp($app)
+    {
+        $app['config']->set('cache.default', 'file');
+
+        $app['config']->set('database.default', 'sqlite');
+        $app['config']->set('database.connections.sqlite', [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+        ]);
     }
 
     protected function getPackageProviders($app)
@@ -24,13 +38,22 @@ class TestCase extends Orchestra
         ];
     }
 
-    public function getEnvironmentSetUp($app)
+    /**
+     * @param  \Illuminate\Foundation\Application  $app
+     */
+    protected function setUpDatabase($app)
     {
-        config()->set('database.default', 'testing');
+        Schema::dropAllTables();
 
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_laravel-user-cache_table.php.stub';
-        $migration->up();
-        */
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->uuid();
+            $table->string('email')->unique();
+            $table->string('name')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->timestamps();
+        });
     }
 }
---
+++ tests/src/Models/User.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Foxws\UserCache\Tests\Models;
+
+use Foxws\UserCache\Tests\Database\Factories\UserFactory;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable implements MustVerifyEmail
+{
+    use HasFactory;
+    use Notifiable;
+
+    protected $guarded = [];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected static function newFactory(): UserFactory
+    {
+        return UserFactory::new();
+    }
+}
