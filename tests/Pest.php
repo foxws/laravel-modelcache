b9d546fdc66fa71c0b5dc5c072db2a00135b9e8c
francoism90 <francoism90@users.noreply.github.com>
2024-10-27 16:11:01 +0000

    Fix styling

--- tests/Pest.php
+++ tests/Pest.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Eloquent\Model;
 
 expect()
-    ->extend('toBeSameModel', fn(Model $model) => $this->is($model)->toBeTrue());
+    ->extend('toBeSameModel', fn (Model $model) => $this->is($model)->toBeTrue());
 
 beforeEach(function () {
     // Fake instances
