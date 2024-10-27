251f3fa39dc1369560dec65c41f55b49511150ca
francoism90 <francoism90@users.noreply.github.com>
2024-10-27 17:23:28 +0100

    wip

--- tests/src/CacheProfiles/CacheAllSuccessful.php
+++ tests/src/CacheProfiles/CacheAllSuccessful.php
@@ -1,7 +1,9 @@
 <?php
 
 use Foxws\UserCache\CacheProfiles\CacheAllSuccessful;
+use Foxws\UserCache\Tests\Models\User;
 
+use function PHPUnit\Framework\assertEquals;
 use function PHPUnit\Framework\assertTrue;
 
 beforeEach(function () {
@@ -11,4 +13,13 @@ beforeEach(function () {
 it('will determine that values should be cached', function () {
     assertTrue($this->cacheProfile->shouldCacheValue('foo'));
     assertTrue($this->cacheProfile->shouldCacheValue(['foo', 'bar']));
+});
+
+it('will use the id of the logged in user to differentiate caches', function () {
+    assertEquals('', $this->cacheProfile->useCacheNameSuffix('foo'));
+
+    User::all()->map(function ($user) {
+        auth()->login(User::find($user->getKey()));
+        assertEquals($user->getKey(), $this->cacheProfile->useCacheNameSuffix('foo'));
+    });
 });
