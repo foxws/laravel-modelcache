db0272f6b35845a4f301a6a6778032fab9d98c3d
francoism90 <francoism90@users.noreply.github.com>
2024-10-30 17:25:01 +0000

    Fix styling

--- tests/src/Models/User.php
+++ tests/src/Models/User.php
@@ -8,7 +8,6 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Support\Facades\Auth;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
