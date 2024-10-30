3c49e2f3f807eb7f7002cdbb921ce79420beede0
François M. <francoism90@users.noreply.github.com>
2024-10-30 18:22:49 +0100

    Add useCacheNameSuffix

--- tests/src/Models/User.php
+++ tests/src/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Auth;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
@@ -26,4 +27,12 @@ class User extends Authenticatable implements MustVerifyEmail
     {
         return UserFactory::new();
     }
+
+    protected function useCacheNameSuffix(string $key): string
+    {
+        if (Auth::check() && Auth::id() === $this->getKey()) {
+            return '';
+        }
+
+        return $this->getKey();
 }
