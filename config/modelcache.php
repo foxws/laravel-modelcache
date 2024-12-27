49414b07171b075e4a4d450cc0fb70796c79be1a
François M. <francoism90@users.noreply.github.com>
2024-12-27 14:10:37 +0100

    Use prefix MODEL_

--- config/modelcache.php
+++ config/modelcache.php
@@ -2,28 +2,26 @@
 
 return [
     /*
-     * Determine if the user cache should be enabled.
+     * Determine if the model cache should be enabled.
      */
-    'enabled' => env('USER_CACHE_ENABLED', true),
+    'enabled' => env('MODEL_CACHE_ENABLED', true),
 
     /*
-     *  The given class will determinate if a request should be cached. The
-     *  default class will cache all successful GET-requests.
-     *
-     *  You can provide your own class given that it implements the
-     *  CacheProfile interface.
+     * The given class will determinate if a value should be cached.
+     * You can provide your own classes given that it implements the
+     * CacheProfile interface.
      */
     'cache_profile' => \Foxws\ModelCache\CacheProfiles\CacheAllSuccessful::class,
 
     /*
      * This must be the name of any store that is configured in config/cache.php.
      */
-    'cache_store' => env('USER_CACHE_STORE') ?: env('CACHE_STORE', 'database'),
+    'cache_store' => env('MODEL_CACHE_STORE') ?: env('CACHE_STORE', 'database'),
 
     /*
      * Default time-to-live for cache items in seconds.
      */
-    'cache_lifetime_in_seconds' => (int) env('USER_CACHE_LIFETIME', 60 * 60 * 24 * 7),
+    'cache_lifetime_in_seconds' => (int) env('MODEL_CACHE_LIFETIME', 60 * 60 * 24 * 7),
 
     /*
      * This class is responsible for generating a hash for a request. This hash
